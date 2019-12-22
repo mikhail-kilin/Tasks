cf546e7dc90cb988e8f99ba05cd4c9369d9cffcd
Mikhail Kilin <1kilinmv1@gmail.com>
2019-12-22 13:29:26 +0300

    added edit comment test

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -20,6 +20,18 @@ describe "As User I want to work with Comments" do
 
     expect(page).to have_content 'Comment shouldn`t be empty'
   end
+
+  scenario "edit comment" do
+    create_task
+    message1 = "message1"
+    create_comment message1
+    message2 = "message2"
+    click_on(class: 'edit')
+    create_comment message2
+    expect(page).to have_content 'Comment was successfully updated.'
+    expect(page).to have_content message2
+    expect(page).to_not have_content message1
+  end
 end
 
 def create_comment(message)
